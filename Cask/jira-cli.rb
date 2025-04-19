da06a262be4b94c09ba7a615de2f48899cf08903
idleberg <idleberg@users.noreply.github.com>
2025-04-19 12:10:14 +0000

    :robot: automatic version bump

--- Cask/jira-cli.rb
+++ Cask/jira-cli.rb
@@ -4,8 +4,8 @@
 cask "jira-cli" do
   arch intel: "x86_64"
 
-  version "1.5.2"
-  sha256 intel: "d1158338225b263c40fb4c60835e9241c135af1049e9dc35061f5a19db9c03f4"
+  version "1.6.0"
+  sha256 intel: "8c6b20d1c3b4a8a09ef85347d8de656ce38da26170727a15660951011487a6ca"
 
   url "https://github.com/ankitpokhrel/jira-cli/releases/download/v#{version}/jira_#{version}_macOS_#{arch}.tar.gz"
   name "Jira CLI"
